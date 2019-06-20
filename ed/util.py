1ff1ba9d5edb5fe6ea24838ab32cf05d6f224793
Eric Smith <ems@fb.com>
2019-06-20 15:13:50 -0400

    Add remaining flag helps

--- ed/util.py
+++ ed/util.py
@@ -67,9 +67,9 @@ def get_parser():
     parser.add_argument("-e", "--epoch-start", type=int, default=0, help='Initial epoch number when resuming training')
     parser.add_argument("-lr", "--learning-rate", type=float, default=None, help='Training learning rate')
     parser.add_argument("--reactonly", action="store_true", help='EmpatheticDialogues: only consider Listener responses')
-    parser.add_argument("--fasttext", type=int, default=None)
-    parser.add_argument("--fasttext-path", type=str, default=None)
-    parser.add_argument("--fasttext-type", type=str, default=None)
+    parser.add_argument("--fasttext", type=int, default=None, help='Number of fastText labels to prepend')
+    parser.add_argument("--fasttext-path", type=str, default=None, help='Path to fastText classifier')
+    parser.add_argument("--fasttext-type", type=str, default=None, help='Specifies labels of fastText classifier')
     return parser
 
 
