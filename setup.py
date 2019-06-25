42bb770fe7589c754d9abee89255ccd3cecd1863
Eric Smith <ems@fb.com>
2019-06-25 09:58:58 -0400

    Fix license file!

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 # Copyright (c) Facebook, Inc. and its affiliates.
-# This source code is licensed under the MIT license found in the
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+#
 
 
 from setuptools import setup, find_packages
