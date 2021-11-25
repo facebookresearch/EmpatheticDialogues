bc14ce2f7b35d14e368e589ca6bade93fb04c1cf
Yashwanth Reddy Gondi <yashwanthreddy.g@live.com>
2021-11-24 17:23:10 -0800

    tokenizer

--- empchat/classifiers/model_lstm.py
+++ empchat/classifiers/model_lstm.py
@@ -1,16 +1,26 @@
+from empchat.datasets.tokens import get_bert_token_mapping
+
 if __name__ == "__main__":
     from data_loader import EmotionDataset
     from torch.utils.data import DataLoader
     from utils import build_word_idx, PAD
     from empchat.datasets.loader import pad
+    from pytorch_pretrained_bert import BertTokenizer
 
     # TODO: provide tokenization function
-    tokenizer = None
+    tokenizer = BertTokenizer.from_pretrained(
+                    "bert-base-cased",
+                    do_lower_case=False,
+                    never_split=(
+                        ["[CLS]", "[MASK]"]
+                        + list(get_bert_token_mapping(None).values())
+                    ),
+                )
 
     # load data
-    train_dataset = EmotionDataset("data/train.csv", True, tokenizer)
-    valid_dataset = EmotionDataset("data/valid.csv", True, tokenizer, label2idx=train_dataset.label2idx)
-    test_dataset = EmotionDataset("data/test.csv", True, tokenizer, label2idx=train_dataset.label2idx)
+    train_dataset = EmotionDataset("../../data/train.csv", True, tokenizer.tokenize)
+    valid_dataset = EmotionDataset("../../data/valid.csv", False, tokenizer.tokenize, label2idx=train_dataset.label2idx)
+    test_dataset = EmotionDataset("../../data/test.csv", False, tokenizer.tokenize, label2idx=train_dataset.label2idx)
 
     word2idx, idx2word, char2idx, idx2char = build_word_idx(
         train_dataset.insts, valid_dataset.insts, test_dataset.insts
@@ -34,10 +44,10 @@ if __name__ == "__main__":
     train_loader = DataLoader(
         train_dataset,
         # TODO: set from CMD
-        # batch_size=opt.batch_size,
-        # shuffle=not opt.no_shuffle,
-        # num_workers=0,
+        batch_size=16,
+        shuffle=True,
+        num_workers=12,
         # see if this works
         collate_fn=batchify,
-        # pin_memory=self.opt.cuda,
+        pin_memory=False,
     )
