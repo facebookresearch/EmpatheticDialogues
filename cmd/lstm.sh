b4c6aed0123b2dbc897254d27003423781b97458
Akshen Kadakia <akshenk8@gmail.com>
2021-11-30 20:20:57 -0800

    training cmds

---
+++ cmd/lstm.sh
@@ -0,0 +1,28 @@
+EMPATHETIC_DIALOGUES_DATA_FOLDER=data
+MODEL_NAME=bert_ft_lstm
+EVAL_SAVE_FOLDER=models/${MODEL_NAME}/eval_save
+TRAIN_SAVE_FOLDER=models/${MODEL_NAME}/train_save
+PRETRAINED_MODEL_PATH=bert_pretrained.mdl
+PATH_TO_TRAINED_FASTTEXT_MODEL=dummy
+
+EMO_MODEL=lstm python retrieval_train.py \
+--batch-size 256 \
+--cuda \
+--bert-dim 300 \
+--dataset-name empchat \
+--dict-max-words 250000 \
+--display-iter 100 \
+--embeddings None \
+--empchat-folder ${EMPATHETIC_DIALOGUES_DATA_FOLDER} \
+--learning-rate 1e-5 \
+--load-checkpoint ${PRETRAINED_MODEL_PATH} \
+--max-hist-len 4 \
+--model bert \
+--model-dir ${TRAIN_SAVE_FOLDER} \
+--model-name ${MODEL_NAME} \
+--num-epochs 100 \
+--optimizer adamax \
+--stop-crit-num-epochs 10 \
+--fasttext 1 \
+--fasttext-path ${PATH_TO_TRAINED_FASTTEXT_MODEL} \
+--fasttext-type emo
